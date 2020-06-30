df4b1c02a14187ea4e1e065805711fd75a00798a
MarcusBuzette <marcusbuzete@me.com>
2020-06-30 10:45:12 -0300

    removed menu music

--- js/scenes/initScene.js
+++ js/scenes/initScene.js
@@ -7,8 +7,8 @@ class InitScene {
     buttons.push(new Button('Ranking', width/2 , height/2 + 75, 'scoreboard'))
     buttons.push(new Button('Controles', width/2 , height/2 + 150, 'controls'))
     buttons.push(new Button('Creditos', width/2 , height/2 + 225, 'credits'))
-    soundTrack.stop()
-    soundTrack.loop()
+    // soundTrack.stop()
+    // soundTrack.loop()
   }
   
   draw() {
